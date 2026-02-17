651bd7dffac623d73deeb0a47418d02a84d54e98
Walter <mejiaanael741@gmail.com.com>
2026-02-17 16:21:15 -0600

    Primer Avance

---
+++ app/src/main/java/com/app/hnconverter/DatabaseHelper.kt
@@ -0,0 +1,23 @@
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, "CurrencyDB", null, 1)
+{
+    override fun onCreate(db: SQLiteDatabase)
+    {
+        db.execSQL("INSERT INTO rates (from_code, to_code, rate) VALUES ('NIO', 'USD', 0.027)")
+        db.execSQL("INSERT INTO rates (from_code, to_code, rate) VALUES ('SVC', 'USD', 0.11)")
+        db.execSQL("CREATE TABLE rates (id INTEGER PRIMARY KEY AUTOINCREMENT, from_code TEXT, to_code TEXT, rate REAL)")
+        db.execSQL("CREATE TABLE conversions (id INTEGER PRIMARY KEY AUTOINCREMENT, from_code TEXT, to_code TEXT, amount REAL, result REAL, date TEXT, is_favorite INTEGER DEFAULT 0)")
+        db.execSQL("INSERT INTO rates (from_code, to_code, rate) VALUES ('HNL', 'USD', 0.040)")
+        db.execSQL("INSERT INTO rates (from_code, to_code, rate) VALUES ('GTQ', 'USD', 0.13)")
+        db.execSQL("INSERT INTO rates (from_code, to_code, rate) VALUES ('CRC', 'USD', 0.0019)")
+    }
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int)
+    {
+        db.execSQL("DROP TABLE IF EXISTS rates")
+        db.execSQL("DROP TABLE IF EXISTS conversions")
+        onCreate(db)
+    }
+}
---
+++ app/src/main/java/com/app/hnconverter/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.app.hnconverter
+
+import DatabaseHelper
+import android.content.ContentValues
+import android.os.Bundle
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var dbHelper: DatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        dbHelper = DatabaseHelper(this)
+
+        //Referencias de los componentes
+        val etAmount = findViewById<EditText>(R.id.editTextNumberDecimal)
+        val spinnerFrom = findViewById<Spinner>(R.id.spinner)
+        val spinnerTo = findViewById<Spinner>(R.id.spinner3) // El segundo spinner para USD
+        val btnConvert = findViewById<Button>(R.id.btnConvert)
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+        val btnHistory = findViewById<Button>(R.id.btnHistory)
+
+        //Llenar el primer Spinner (Centroamerica)
+        val monedasCA = arrayOf("HNL", "GTQ", "CRC", "NIO", "SVC")
+        val adapterCA = ArrayAdapter(this, android.R.layout.simple_spinner_item, monedasCA)
+        adapterCA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerFrom.adapter = adapterCA
+
+        // 3 Llenar el segundo Spinner (USD)
+        val monedaDestino = arrayOf("USD")
+        val adapterUSD = ArrayAdapter(this, android.R.layout.simple_spinner_item, monedaDestino)
+        adapterUSD.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerTo.adapter = adapterUSD
+
+        //Configurar eventos de botones
+        btnConvert.setOnClickListener {
+            realizarConversion(etAmount, spinnerFrom, tvResult)
+        }
+
+        btnHistory.setOnClickListener {
+            Toast.makeText(this, "Abriendo historial...", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun realizarConversion(et: EditText, spinner: Spinner, tv: TextView) {
+        val montoStr = et.text.toString()
+        if (montoStr.isEmpty()) {
+            et.error = "Escribe un monto"
+            return
+        }
+
+        val monto = montoStr.toDouble()
+        val moneda = spinner.selectedItem.toString()
+
+        val db = dbHelper.readableDatabase
+        val cursor = db.rawQuery("SELECT rate FROM rates WHERE from_code = ?", arrayOf(moneda))
+
+        if (cursor.moveToFirst()) {
+            val tasa = cursor.getDouble(0)
+            val resultado = monto * tasa
+
+            // Mostrar resultado en pantalla
+            tv.text = String.format("%.2f USD", resultado)
+
+            // Guardar en SQLite
+            guardarEnHistorial(moneda, "USD", monto, resultado)
+            Toast.makeText(this, "Conversión guardada en SQLite", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Error: Tasa no encontrada en la BD", Toast.LENGTH_SHORT).show()
+        }
+        cursor.close()
+    }
+
+    private fun guardarEnHistorial(from: String, to: String, amount: Double, result: Double) {
+        val db = dbHelper.writableDatabase
+        val values = ContentValues().apply {
+            put("from_code", from)
+            put("to_code", to)
+            put("amount", amount)
+            put("result", result)
+            put("date", SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault()).format(Date()))
+        }
+        db.insert("conversions", null, values)
+    }
+}
